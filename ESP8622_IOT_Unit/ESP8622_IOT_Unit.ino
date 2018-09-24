6c74bdfdfd0673ab9744d95edd442ff8449806e1
Jesper Ahasverusen Kielsgaard <jkielsgaard@outlook.com>
2018-09-24 21:15:37 +0200

    DHT22 sensor and realtime NTP timstamp is now added

--- ESP8622_IOT_Unit/ESP8622_IOT_Unit.ino
+++ ESP8622_IOT_Unit/ESP8622_IOT_Unit.ino
@@ -1,30 +1,44 @@
+
+#include "DHT.h"
 #include <EasyNTPClient.h>
 #include <WiFiUdp.h>
-
 #include <HttpClient.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
 
+// DHT22 data pin on digitalpin 5(14)
+#define DHTPIN 14
+#define DHTTYPE DHT22 
+DHT dht(DHTPIN, DHTTYPE);
+
+
+// Wifi credentials, Cloud host url, API key to the cloud API and unitID
 char ssid[] = "<SSID>";
 char pass[] = "<PASSWORD>";
 String hostURL = "<HOSTURL>";
 String Apikey = "<APIKEY>";
+String unitID = "<UNITID>";
 
 
+// WiFiUDP and EasyNTPC is to collect the unix timestamp
 WiFiUDP ntpUDP;
 EasyNTPClient ntpClient(ntpUDP, "pool.ntp.org", 7200);
 
 
+// Setup LED and serial monitor port
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(74880);
+  Serial.begin(115200);
+  dht.begin();
   delay(100);
-  
+
   ConnectWifi();
 }
 
+
+// Setup the Wifi connection and await to connect 
 void ConnectWifi()
 {
   WiFi.mode(WIFI_STA);
@@ -32,33 +46,61 @@ void ConnectWifi()
   delay(100);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    Serial.print("."); 
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(1000); 
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(1000);
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("WiFi connected - IP: ");
   Serial.println(WiFi.localIP());
 }
 
 
+// Start loop, will blink with the LED se user can see the unit active and in the loop
 void loop(){
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000); 
   digitalWrite(LED_BUILTIN, HIGH); 
   delay(2000); 
-   
-  post();
+
+  // check wifi status, to ensure no error when sending climatedata 
+  if(WiFi.status() == WL_CONNECTED) { post(); }
+  else { Serial.println("Lost connection... ☠"); }
 
   delay(10000); 
-  //delay(600000);   
 }
 
+
+// The HTTP post function to send the climatedata to the cloud
 void post()
 {
-  String unitID = "7560f9";
+  HTTPClient http; 
+  http.begin(hostURL, "2D:A5:86:8A:33:0B:81:91:EB:58:55:D7:BD:C6:3D:1B:EB:B3:92:B3");
+  http.addHeader("x-api-key", Apikey);
+  http.addHeader("Content-Type", "application/json");
+  int httpCode = http.POST(ClimateDATA());
+  if(httpCode > 0) {
+    if(httpCode == HTTP_CODE_OK) {
+      Serial.println("✉+☁=✔");
+    } else {
+    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    Serial.println(httpCode);
+    }
+  } else {
+    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+}
+
+
+// The DHT22 ClimateDATA function to collect the data
+String ClimateDATA()
+{
   long datetime = ntpClient.getUnixTime();
+  float t = 0;
+  float h = 0;
   
   StaticJsonBuffer<300> JSONbuffer;
   JsonObject& JSONencoder = JSONbuffer.createObject();
@@ -69,25 +111,12 @@ void post()
   Item["datestamp"] = datetime;
   Item["unit"] = unitID;
   JsonObject& climatedata = Item.createNestedObject("climatedata");
-  climatedata["humidity"] = 50;
-  climatedata["temperature"] = 23;
+  climatedata["humidity"] = h = dht.readHumidity();
+  climatedata["temperature"] = t = dht.readTemperature();
+  climatedata["heatindex"] = dht.computeHeatIndex(t, h, false);
   
   char JSONmessageBuffer[300];
   JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-  
-  HTTPClient http; 
-  http.begin(hostURL, "2D:A5:86:8A:33:0B:81:91:EB:58:55:D7:BD:C6:3D:1B:EB:B3:92:B3");
-  http.addHeader("x-api-key", Apikey);
-  http.addHeader("Content-Type", "application/json");
-  int httpCode = http.POST(JSONmessageBuffer);
-  if(httpCode > 0) {
-    //Serial.printf("[HTTP] POST... code: %d\n", httpCode);
-    if(httpCode == HTTP_CODE_OK) {
-      //String payload = http.getString();
-      Serial.println("✉+☁=✔");
-    }
-  } else {
-    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
-  }
-  http.end();
+
+  return JSONmessageBuffer;
 }
