a0c420691286d2e6e4d3dfd7edf6792bbdf90a9c
Jesper Ahasverusen Kielsgaard <jkielsgaard@outlook.com>
2018-10-10 13:49:05 +0200

    small chenge.

--- ESP8622_IOT_Unit/ESP8622_IOT_Unit.ino
+++ ESP8622_IOT_Unit/ESP8622_IOT_Unit.ino
@@ -17,10 +17,12 @@ DHT dht(DHTPIN, DHTTYPE);
 // Wifi credentials, Cloud host url, API key to the cloud API and unitID
 char ssid[] = "<SSID>";
 char pass[] = "<PASSWORD>";
-String datadbURL= = "<HOSTURL>";
+String datadbURL = "<HOSTURL>";
 String apikey = "<APIKEY>";
 String unitid = "<UNITID>";
+String SHA1fingerprint = "<SHA1KEY>"
 
+int counter = 0;
 
 // WiFiUDP and EasyNTPC is to collect the unix timestamp
 WiFiUDP ntpUDP;
@@ -81,44 +83,49 @@ void post()
   while (attempt < 3)
   {
     HTTPClient http; 
-    http.begin(datadbURL, "2D:A5:86:8A:33:0B:81:91:EB:58:55:D7:BD:C6:3D:1B:EB:B3:92:B3");
+    http.begin(datadbURL, SHA1fingerprint);
     http.addHeader("x-api-key", apikey);
     http.addHeader("Content-Type", "application/json");
     int httpCode = http.POST(ClimateDATA());
     if(httpCode > 0) {
       if(httpCode == HTTP_CODE_OK) {
+        Serial.print(counter);
         Serial.println("✉+☁=✔");
         attempt = 3;
-      } else { Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str()); delay(2000); }
-    } else { Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str()); delay(2000); }
+      } else { Serial.printf("[HTTP_CODE_FAIL] POST... failed, error: %s\n", http.errorToString(httpCode).c_str()); delay(2000); }
+    } else { Serial.printf("[HTTP_ERROR] POST... failed, error: %s\n", http.errorToString(httpCode).c_str()); delay(2000); }
     attempt++;
     http.end();
   }
+  counter++;
 }
 
 
 // The DHT22 ClimateDATA function to collect the data
 String ClimateDATA()
 {
-  long datetime = ntpClient.getUnixTime();
-  float t = 0;
-  float h = 0;
+  long datetime = 0;
+  while(datetime == 0) { datetime = ntpClient.getUnixTime(); }
+  
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+
+  if(isnan(t)) { t = dht.readTemperature(); }
+  if(isnan(h)) { h = dht.readHumidity(); }
   
   StaticJsonBuffer<300> JSONbuffer;
   JsonObject& JSONencoder = JSONbuffer.createObject();
-  
-  JSONencoder["TableName"] = "climatedata";
-  JsonObject& Item = JSONencoder.createNestedObject("Item");
-  Item["id"] = unitid + "_" + datetime;
-  Item["datestamp"] = datetime;
-  Item["unit"] = unitid;
-  JsonObject& climatedata = Item.createNestedObject("climatedata");
-  climatedata["humidity"] = h = dht.readHumidity();
-  climatedata["temperature"] = t = dht.readTemperature();
+  JSONencoder["datestamp"] = datetime;
+  JSONencoder["unitid"] = unitid;
+  JsonObject& climatedata = JSONencoder.createNestedObject("climatedata");
+  climatedata["temperature"] = t;
+  climatedata["humidity"] = h;
   climatedata["heatindex"] = dht.computeHeatIndex(t, h, false);
   
   char JSONmessageBuffer[300];
   JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+
+  //Serial.print(JSONmessageBuffer); for testing
 
   return JSONmessageBuffer;
 }
